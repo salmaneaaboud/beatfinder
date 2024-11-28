9a64cfe5d19efdb28e9bd71bf2f01409f52585b6
CrushedDEV <rojito7676@gmail.com>
2024-11-28 19:05:57 +0100

    Doing login

---
+++ src/pages/Guest/Login/Login.jsx
@@ -0,0 +1,24 @@
+import './Login.css';
+import {Header} from '/src/components/Header/Header.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import Form from '/src/components/';
+
+function Login() {
+  return (
+    <>
+<Header/>
+<Container>
+    <Row>
+        <Col>   
+                </Form>
+        </Col>
+    </Row>
+</Container>
+
+
+    </>
+  );
+};
+
+export default Login;
