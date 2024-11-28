94621075bc1415480f073ea7ae77b7708a624955
CrushedDEV <rojito7676@gmail.com>
2024-11-28 20:50:07 +0100

    Login and register page completed, header css change button padding

--- src/pages/Guest/Register/Register.jsx
+++ src/pages/Guest/Register/Register.jsx
@@ -1,16 +1,22 @@
-import './Login.css';
+import './Register.css';
 import { Header } from '/src/components/Header/Header.jsx';
 import { Container, Row, Col } from "react-bootstrap";
 import Form from '/src/components/Form/Form';
 import SocialButton from '/src/components/SocialButton/SocialButton';
-import {CustomButton} from '/src/components/CustomButton/CustomButton';
 
 const campos = [
   {
-    name: 'emailOrUsername',
-    label: 'Correo electrónico o nombre de usuario',
+    name: 'email',
+    label: 'Correo electrónico ',
     type: 'text',
-    placeholder: 'Ingresa tu correo o usuario',
+    placeholder: 'Ingresa tu correo',
+    required: true,
+  },
+  {
+    name: 'username',
+    label: 'Nombre de usuario',
+    type: 'text',
+    placeholder: 'Ingresa tu nombre de usuario',
     required: true,
   },
   {
@@ -20,6 +26,13 @@ const campos = [
     placeholder: 'Ingresa tu contraseña',
     required: true,
   },
+  {
+    name: 'confirmPassword',
+    label: 'Confirmar contraseña',
+    type: 'password',
+    placeholder: 'Confirma tu contraseña',
+    required: true,
+  },
 ];
 
 const handleSubmit = (e) => {
@@ -27,34 +40,29 @@ const handleSubmit = (e) => {
   console.log('Formulario enviado');
 };
 
-function Login() {
+function Register() {
   return (
     <>
       <Header />
-      <Container className="login-container">
+      <Container className="register-container">
         <Row className="justify-content-center">
           <Col lg={6} xl={5}>
-            <div className="login-form">
-              <h2>Conectarse</h2>
-              <div className='login-center'>
-              <div className="login-toggle-buttons">
-                <CustomButton type='' value='Cliente'/>
-                <CustomButton type='btn-outline-light' value='Productor'/>
+            <div className="register-form">
+              <h2>Crear cuenta</h2>
+              <div className='register-center'>
+                <Form
+                  title=""
+                  campos={campos}
+                  onSubmit={handleSubmit}
+                  botonTexto="Registrarse"
+                />
               </div>
-              </div>
-
-              <Form
-                title=""
-                campos={campos}
-                onSubmit={handleSubmit}
-                botonTexto="Iniciar sesión"
-              />
-              <div className="social-login">
+              <p className="login-text">¿Ya tienes una cuenta Beatfinder? <a href="/login">Inicia sesión</a></p>
+              <div className="social-register">
                 <SocialButton icon='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png' url='#'/>
                 <SocialButton icon='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/2048px-2021_Facebook_icon.svg.png' url='#'/>
                 <SocialButton icon='https://www.svgrepo.com/show/442910/brand-apple.svg' url='#'/>
               </div>
-              <p className="register-text">¿Todavía no tienes una cuenta Beatfinder? <a href="/register">Regístrate</a></p>
             </div>
           </Col>
         </Row>
@@ -63,4 +71,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Register;
