0ee77ebf31caf70406412dfe869624ecbac3b3bb
DW32 TALDEA <ik012982i10@H012982.NET>
2025-01-08 09:21:09 +0100

    header fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import logo from "/src/assets/logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Header.css";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 export function Header() {
   const { i18n, t } = useTranslation();
@@ -21,24 +21,42 @@ export function Header() {
 
         <div className="d-flex gap-3">
           <Dropdown align="end">
-            <Dropdown.Toggle variant="light" className="client-dropdown" id="dropdown-basic">
+            <Dropdown.Toggle
+              variant="light"
+              className="client-dropdown"
+              id="dropdown-basic"
+            >
               Idioma
             </Dropdown.Toggle>
             <Dropdown.Menu className="custom-dropdown-menu">
-              <Dropdown.Item onClick={() => changeLanguage("es")}>Español</Dropdown.Item>
-              <Dropdown.Item onClick={() => changeLanguage("en")}>English</Dropdown.Item>
+                <Dropdown.Item
+                  onClick={() => changeLanguage("es")}
+                  style={{
+                    fontWeight: i18n.language === "es" ? "bold" : "normal",
+                  }}
+                >
+                  Español
+                </Dropdown.Item>
+                <Dropdown.Item
+                  onClick={() => changeLanguage("en")}
+                  style={{
+                    fontWeight: i18n.language === "en" ? "bold" : "normal",
+                  }}
+                >
+                  English
+                </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
-          
+
           <Link to="/register">
-            <Button variant="outline-light" className="btn-register">
+            <Button variant="primary" className="btn-register">
               {t("header.register_button")}
             </Button>
           </Link>
-          
+
           <Link to="/login">
-            <Button variant="primary" className="btn-login">
-            {t("header.login_button")}
+            <Button variant="outline-light" className="btn-login">
+              {t("header.login_button")}
             </Button>
           </Link>
         </div>
