fa3c02f5d87564c4468f0f34c1b9629a7b5ba868
DW32 TALDEA <ik012982i10@H012982.NET>
2025-02-08 23:44:06 +0100

    filters toggler on discover

--- src/pages/Client/Discover/Discover.jsx
+++ src/pages/Client/Discover/Discover.jsx
@@ -9,6 +9,7 @@ import { useState } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import ReactPaginate from 'react-paginate';
+import { FaFilter, FaEye } from "react-icons/fa"; // Importar iconos
 
 const Discover = () => {
     const { activeSong, isPlaying } = useSelector((state) => state.player);
@@ -20,6 +21,7 @@ const Discover = () => {
     const [selectedKey, setSelectedKey] = useState([]);
     const [selectedPrice, setSelectedPrice] = useState([0, 1000]);
     const [currentPage, setCurrentPage] = useState(0);
+    const [filtersVisible, setFiltersVisible] = useState(false); // Estado para los filtros
     const songsPerPage = 8;
 
     const filteredSongs = (data || []).filter(song => {
@@ -61,28 +63,66 @@ const Discover = () => {
                     <Loader title="Cargando canciones..." />
                 ) : (
                     <div className="d-flex flex-column mx-auto" style={{ width: "90%" }}>
-                        <div className="genre-filters d-flex flex-wrap justify-content-center mb-4">
-                            {uniqueGenres.map((genre) => (
-                                <button
-                                    key={genre}
-                                    onClick={() => handleGenreFilter(genre)}
-                                    className={`genre-button ${selectedGenres.includes(genre) ? "active" : ""}`}
-                                    style={{
-                                        padding: "10px 20px",
-                                        margin: "5px",
-                                        border: "none",
-                                        borderRadius: "20px",
-                                        backgroundColor: selectedGenres.includes(genre) ? "#8C52FF" : "#f0f0f0",
-                                        color: selectedGenres.includes(genre) ? "#fff" : "#000",
-                                        cursor: "pointer",
-                                        transition: "background-color 0.3s",
-                                    }}
-                                >
-                                    {genre}
-                                </button>
-                            ))}
+                        <div className="d-flex justify-content-between align-items-center mb-3">
+                            <button
+                                onClick={() => setFiltersVisible(!filtersVisible)}
+                                className="btn btn-transparent"
+                                style={{
+                                    padding: "10px 20px",
+                                    borderRadius: "20px",
+                                    backgroundColor: "transparent",
+                                    color: "white",
+                                    border: "2px solid white",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    transition: "background-color 0.3s ease",
+                                }}
+                            >
+                                {filtersVisible ? (
+                                    <FaEye style={{ marginRight: "8px" }} />
+                                ) : (
+                                    <FaFilter style={{ marginRight: "8px" }} />
+                                )}
+                                {filtersVisible ? "Ocultar filtros" : "Mostrar filtros"}
+                            </button>
                         </div>
-                        <div className="filter-section d-flex flex-column align-items-center mb-4">
+
+                        <div
+                            className={`filter-section d-flex flex-column align-items-center mb-4 ${
+                                filtersVisible ? "visible" : "hidden"
+                            }`}
+                            style={{
+                                maxHeight: filtersVisible ? "800px" : "0",
+                                opacity: filtersVisible ? 1 : 0,
+                                overflow: "hidden",
+                                transition: "max-height 0.5s ease, opacity 0.5s ease",
+                            }}
+                        >
+                            <div className="genre-filters gap-3 d-flex flex-wrap justify-content-center align-items-center mb-4 text-white">
+                                <span>Género: </span>
+                                <div>
+                                    {uniqueGenres.map((genre) => (
+                                        <button
+                                            key={genre}
+                                            onClick={() => handleGenreFilter(genre)}
+                                            className={`genre-button ${selectedGenres.includes(genre) ? "active" : ""}`}
+                                            style={{
+                                                padding: "10px 20px",
+                                                margin: "5px",
+                                                border: "none",
+                                                borderRadius: "20px",
+                                                backgroundColor: selectedGenres.includes(genre) ? "#8C52FF" : "#f0f0f0",
+                                                color: selectedGenres.includes(genre) ? "#fff" : "#000",
+                                                cursor: "pointer",
+                                                transition: "background-color 0.3s",
+                                            }}
+                                        >
+                                            {genre}
+                                        </button>
+                                    ))}
+                                </div>
+                            </div>
+
                             <div className="d-flex flex-column align-items-center mb-3 text-white">
                                 <span>BPM: {selectedBpm[0]} - {selectedBpm[1]}</span>
                                 <Slider
@@ -134,6 +174,7 @@ const Discover = () => {
                                 />
                             </div>
                         </div>
+
                         <div className="d-flex flex-wrap justify-flex-start gap-4 mb-4">
                             {currentSongs?.map((song, i) => (
                                 <SongCard
