483409f0a45c57da693885396ac98f59ddb55f93
OihanGomez <ogomezaz23dw@ikzubirimanteo.com>
2025-02-09 18:02:03 +0100

    a

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import './Card.css';;
+import './Card.css';
 import PropTypes from 'prop-types';
 
-
-function Card({ title, subtitle, imageURL, detailsURL} ) {
+function Card({ title, subtitle, imageURL, detailsURL }) {
   return (
-
-<div className='custom-card'>
-    <div className='card-image'>
+    <div className='profile-card-container'>
+      <div className='profile-card-image'>
         <img src={imageURL} alt={title} />
-    </div>
-    <div className='card-info'>
-        <div className='card-title'>
-            <h3>{title}</h3>
-            <p>{subtitle}</p>
+      </div>
+      <div className='profile-card-info'>
+        <div className='profile-card-title'>
+          <h3>{title}</h3>
+          <p>{subtitle}</p>
         </div>
         <a href={detailsURL}><p> &gt; See Details</p></a>
-
+      </div>
     </div>
-</div>
-
   );
 }
+
 Card.propTypes = {
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
   imageURL: PropTypes.string.isRequired,
   detailsURL: PropTypes.string
 };
+
 export default Card;
